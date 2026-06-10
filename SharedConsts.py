0757ff6b918484975ad966e078e1ffe61e652d10
XseniaP <50076292+XseniaP@users.noreply.github.com>
2026-06-10 01:58:24 -0400

    upd SharedConsts.py

--- SharedConsts.py
+++ SharedConsts.py
@@ -10,12 +10,12 @@ from enum import Enum
 import platform
 
 # constants to use when sending e-mails using the server admin's email address.
-ADMIN_EMAIL = 'evolseq@gmail.com' #'TAU Evolseq <evolseq@tauex.tau.ac.il>'
+ADMIN_EMAIL = 'evolseq@tauex.tau.ac.il'
 DEV_EMAIL = 'josefspr@gmail.com'
-SMTP_SERVER = 'smtp.gmail.com:587' #'mxout.tau.ac.il'
+SMTP_SERVER = 'mxout.tau.ac.il'
 ADMIN_USER_NAME = 'evolseq'
-ADMIN_PASSWORD = 'yipnqomnsofhytqp'
-SEND_EMAIL_DIR_IBIS = '/home/josefspr/bioseq/bioSequence_scripts_and_constants/sendEmail'
+ADMIN_PASSWORD = 'elana'
+SEND_EMAIL_DIR_IBIS = '/guidance/perl/'
 
 OWNER_EMAIL = 'josefspr@gmail.com'
 
@@ -39,14 +39,15 @@ FAILED_STATUS_TAG = '''<H1 align=center>Pepitope Job Status Page - <font color='
 
 WEBSERVER_NAME_CAPITAL = 'Guidance'
 WEBSERVER_NAME = 'guidance'
-WEBSERVER_URL = '/guidance' #f'http://{WEBSERVER_NAME}.tau.ac.il'
-WEBSERVER_URL_EXT = 'https://taux.evolseq.net/guidance'
+WEBSERVER_URL = 'http://dev.guidance.tau.ac.il' #f'http://{WEBSERVER_NAME}.tau.ac.il'
+WEBSERVER_URL_EXT = 'http://dev.guidance.tau.ac.il'
 WEBSERVER_OLD_URL = f'http://{WEBSERVER_NAME}-old.tau.ac.il'
 WEBSERVER_TITLE = '<b>Server for a multiple sequence alignment confidence score calculation</b>'
 
-WEBSERVER_RESULTS_DIR = os.path.join(SERVERS_RESULTS_DIR, 'Guidance')
-WEBSERVER_LOGS_DIR = os.path.join(SERVERS_LOGS_DIR, 'Guidance')
+WEBSERVER_RESULTS_DIR = SERVERS_RESULTS_DIR #os.path.join(SERVERS_RESULTS_DIR, 'guidance')
+WEBSERVER_LOGS_DIR = SERVERS_LOGS_DIR #os.path.join(SERVERS_RESULTS_DIR, 'guidance', 'logs')
 WEBSERVER_HTML_DIR = f'/var/www/html/{WEBSERVER_NAME}/ver2'
+
 
 WEBSERVER_RESULTS_URL = os.path.join(WEBSERVER_URL, 'results')
 WEBSERVER_LOG_URL = os.path.join(WEBSERVER_URL, 'logs')
@@ -63,6 +64,7 @@ SOURCES = '/source.php'
 
 SCRIPTS_DIR = os.path.join(BIN_DIR, 'script')
 MAIN_SCRIPT = os.path.join(SCRIPTS_DIR, 'guidance_main.py')
+EMAIL_SCRIPT = os.path.join(SCRIPTS_DIR, 'email_sender.py')
 MASK_SCRIPT = os.path.join(SCRIPTS_DIR, 'mask_low_score_residues_webserver.py')
 REMOVE_POS_SCRIPT = os.path.join(SCRIPTS_DIR, 'remove_pos_below_cutoff.py')
 REMOVE_SEQ_SCRIPT = os.path.join(SCRIPTS_DIR, 'remove_seq_below_cutoff.py')
@@ -73,7 +75,7 @@ MAFFT_OP_DIST_0_25 = os.path.join(SCRIPTS_DIR, 'balibase.mafft_7123_mafft.op2.Di
 MAFFT_EP_DIST_0_25 = os.path.join(SCRIPTS_DIR, 'balibase.mafft_7123_mafft.ep2.Dist20bins.txt')
 HOT_GUIDANCE2_PROGRAM = os.path.join(SCRIPTS_DIR, 'hot_cos_main.py')
 MIDPOINT_ROOTING_R = os.path.join(SCRIPTS_DIR, 'programs', 'MidPoint_Rooting.R')
-MSA_Score_CSS = "https://taux.evolseq.net/static/css/MSA_Colored.NEW.EM.css"
+MSA_Score_CSS = "http://dev.guidance.tau.ac.il/static/css/MSA_Colored.NEW.EM.css"
 MidPoint_Rooting_R = os.path.join(SCRIPTS_DIR, 'programs', 'MidPoint_Rooting.R')
 
 if platform.system()=='Darwin': # macOS platform
@@ -129,7 +131,7 @@ GUIDANCE_RUNNING_JOBS = f'{EXTERAL_SCRIPTS_PATH}/guidance_running_jobs.list'
 JOB_QUEUE_NAME =  'pupkowebr@power9' #'itaym'
 
 
-SUBMISSIONS_LOG = os.path.join(SERVERS_LOGS_DIR,WEBSERVER_NAME_CAPITAL, 'guidance.logv2')
+SUBMISSIONS_LOG =  os.path.join(WEBSERVER_LOGS_DIR, 'guidance.logv2')
 RESULT_WEBPAGE_NAME = 'output.html'
 EMAIL_FILE_NAME = 'email.txt'
 
@@ -153,8 +155,8 @@ PROGRESS_BAR_TAG = '''<div class="progress"><div class="progress-bar progress-ba
 MODE_0755 = stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH
 
 DAILY_TEST_SEQUENCE = ">NC_001802.HXB2\nMQPIPIVAIVALVVAIIIAIVVWSIVIIEYRKILRQRKIDRLIDRLIERAEDSGNESEGEISALVEMGVEMGHHAPWDVDDL\n>EF637049.B\nMQSLQIVAIVALVVTAIIAIVVWSIVLIEYRKLLRQRKIDRLIDRIRERAEDSGNESEGDQEELAGLVERGHLAPWDVDDL\n>EF514700.B\nMQPLEILAIVALVVAIILAIVVWTIVFIEYKKILRQRKIDRLIDRIAERAEDSGNESEGDQEELSALVDMGHDAPWVVVDQ\n>DQ056417.C\nMLESIDYRLGVAALLLALIIAIIVWIIAYLEYRKLLRQRRIDKLIKRIRERAEDSGNESEGDIEELSTMVDVEHLRLLDVNNL\n>AY463217.C\nMVDLLAGVDYRVGVGALIIALIIAIIVWIWVYIEYRKLLRQRKIDWLIKRLREREEDSGNESEGDTEELATMVDMGHLRLLDDNNV\n>DQ011165.C\nMLNFLAGVDYRIGVGALIVGLIIAIVVWIIVYLEYRKLVKQRKIDWLIERIRERAEDSGNESEGDTEELATMVDMGHLRLLDAYDL\n>AB254142.C\nMINFAARVDYRVGVAAFTIALIIAIVVWIIVYLELVRQRKIDQLIIRIREREEDSGNESEGDIEELSTMVDMGQLRLLDGNGL\n>AY901969.C\nMVNLLEKVNLFEKVDYRLGVGALLIALVIAIIVWTIAYIEYRKLVRQRKIDWLVKRIRERAEDSGNESDGDTEELSTMVDLGHLRLLDVAEL\n>EU110088.A1\nMNQLQILAIXGLVVALILAIVVWTIVGIEYRKLLRQRRIDRLIKRISERAEDSGNESDGDTEELSQLVEMGNYNLGFDDNL\n>AB253428.A1\nMQLLEICAVVGLVVALIIAIVVWTIVGIEYKKLLKQRKIDRLVDRIRERAEDSGNESDGDREELSLLVDMGDYDLGDDNNL\n>AF457052.A1\nMLSALEICAIAGLVIALIIAIVVWTIVGIEYRRLLKQRKIDRLIERIRERAEDSGNESDGDTEELAALIEMGNYDLGDANDL\n>AF077336.F1\nMSYLLAIGIAALIVALIIAIVVWTIVYIEYKKLVRQRKINKLYKRIRERAEDSGNESEGDAEELAALGEMGPFIPGDINNL\n>DQ168575.G\nMKSLEISAIVGLIVAFIAAIVVWTIVLIEYRKIRKQKRIDKILDRIRERAEDSGNESEGDTEELATLVDMVDFEPWVGDNL\n>AY795907.D\nMQTLEILSIVALVIAAIIAIIVWTIVYIEYRKIRRQRKIDQLIDRIRERAEDSGNESEGDEEELSTLMEMGHAAPWNVADDL\n"
-WRITE_DAILY_TEST_SCRIPT = f"{Bin}/write_daily_test_flask.py"
-DAILY_TEST_DIR = "/home/josefspr/bioseq/bioSequence_scripts_and_constants/daily_tests/"
+WRITE_DAILY_TEST_SCRIPT = f"/guidance/script/write_daily_test_flask.py"
+DAILY_TEST_DIR = "/guidance/daily_tests"
 
 # from SharedConstants
 class EMAIL_CONSTS:
@@ -192,9 +194,9 @@ NEW_RUNNING_JOBS_NAME = 'NewRunning'
 FINISHED_JOBS_NAME = 'Finished'
 ERROR_JOBS_NAME = 'Error'
 WEIRD_BEHAVIOR_JOB_TO_CHECK = ''
-PATH2SAVE_PROCESS_DICT = r'SavedObjects/processes.dict'
-PATH2SAVE_WAITING_LIST = r'SavedObjects/waiting.lst'
-PATH2SAVE_PREVIOUS_DF = r'SavedObjects/previous_processes.csv'
+PATH2SAVE_PROCESS_DICT = r'/var/www/vhosts/dev.guidance.tau.ac.il/httpdocs/SavedObjects/processes.dict'
+PATH2SAVE_WAITING_LIST = r'/var/www/vhosts/dev.guidance.tau.ac.il/httpdocs/SavedObjects/waiting.lst'
+PATH2SAVE_PREVIOUS_DF = r'/var/www/vhosts/dev.guidance.tau.ac.il/httpdocs/SavedObjects/previous_processes.csv'
 
 # PBS Listener consts
 JOB_RUNNING_TIME_LIMIT_IN_HOURS = 10
